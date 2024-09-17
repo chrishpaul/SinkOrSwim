1ac4c33aed42935c073dec283ea2e21289f94ef4
chrishpaul <87882234+chrishpaul@users.noreply.github.com>
2024-09-17 13:50:25 -0700

    Incorporated reading from JSON and modified functions in model to use new data structure.

---
+++ SinkOrSwim/LessonWord.h
@@ -0,0 +1,16 @@
+//
+//  LessonWord.h
+//  SinkOrSwim
+//
+//  Created by Chrishnika Paul on 9/17/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LessonWord : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
