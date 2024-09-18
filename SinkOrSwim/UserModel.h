50c85f8939c603bbfad45c6277570b184ae40c92
chrishpaul <87882234+chrishpaul@users.noreply.github.com>
2024-09-18 14:39:27 -0700

    Added 2 additional dynamic cell types, including Logo cell and Pinyin cell

---
+++ SinkOrSwim/UserModel.h
@@ -0,0 +1,16 @@
+//
+//  UserModel.h
+//  SinkOrSwim
+//
+//  Created by Chrishnika Paul on 9/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SinkOrSwim/UserModel.m
@@ -0,0 +1,12 @@
+//
+//  UserModel.m
+//  SinkOrSwim
+//
+//  Created by Chrishnika Paul on 9/18/24.
+//
+
+#import "UserModel.h"
+
+@implementation UserModel
+
+@end
