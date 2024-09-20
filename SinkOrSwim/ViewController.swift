d80761eb9bad950e0efc98689d9f63ca356ab266
chrishpaul <87882234+chrishpaul@users.noreply.github.com>
2024-09-20 13:12:46 -0700

    Cleaned up code for ViewController and added comments

--- SinkOrSwim/ViewController.swift
+++ SinkOrSwim/ViewController.swift
@@ -8,38 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // View Controller for Word Review view
     
+    // MARK: Outlets
     @IBOutlet weak var mandarinLabel: UILabel!
     @IBOutlet weak var pinyinLabel: UILabel!
     @IBOutlet weak var englishLabel: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var wordNavigation: UIStepper!
+    
+    // MARK: Variables
     var englishWord = "English"
    
     lazy var mandarinModel: MandarinModel = {
         return MandarinModel.sharedInstance()
     }()
     
-    /*
-    lazy private var imageView: UIImageView? = {
-        return UIImageView.init(image: self.mandarinModel.getPicForEnglishWord(englishWord))
-    }()
-    */
-    
-    /*lazy var wordIndex: Int = {
-        return MandarinModel.sharedInstance().getIndexOfWord(englishWord)
-    }()*/
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        setWords()
+
+        setWords()  //Set initial values to display
+        
+        // Parameters for UIStepper
         wordNavigation.maximumValue = Double(self.mandarinModel.numberOfWords() - 1)
         wordNavigation.value = Double(self.mandarinModel.getIndexOfWord(englishWord))
     }
     
     func setWords() {
+        // Function to setup view
         englishLabel.text = englishWord
         mandarinLabel.text = self.mandarinModel.getMandarinForEnglish(englishWord)
         pinyinLabel.text = self.mandarinModel.getPinyinForEnglish(englishWord)
@@ -47,24 +43,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func switchChanged(_ sender: UISegmentedControl) {
+        // Function that handles segmented control selection
         switch sender.selectedSegmentIndex{
-        case 0:
+        case 0:                                 //All languages
             mandarinLabel.isHidden = false
             englishLabel.isHidden = false
             pinyinLabel.isHidden = false
-        case 1:
+        case 1:                                 //Mandarin only
             mandarinLabel.isHidden = false
             englishLabel.isHidden = true
             pinyinLabel.isHidden = true
-        case 2:
+        case 2:                                 //Pinyin only
             mandarinLabel.isHidden = true
             englishLabel.isHidden = true
             pinyinLabel.isHidden = false
-        case 3:
+        case 3:                                 //English only
             mandarinLabel.isHidden = true
             englishLabel.isHidden = false
             pinyinLabel.isHidden = true
-        default:
+        default:                                //Default to all
             mandarinLabel.isHidden = false
             englishLabel.isHidden = false
             pinyinLabel.isHidden = false
@@ -72,10 +69,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func changeWord(_ sender: UIStepper) {
+        // Function to change word selection based on stepper
         let index = Int(sender.value)
         englishWord = self.mandarinModel.getEnglishWord(at: index)
-        setWords()
+        setWords()      //Call to update view
     }
-    
 }
 
